271a0f6689767a61f8e42ba2880aa4c729270dcd
camilaborinsky <cborinsky@itba.edu.ar>
2022-04-02 08:33:53 -0300

    set up styles

---
+++ cashop_frontend/lib/style/color_palette.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  static const Color algaeGreen = Color(0xff84d9b1);
+  static const Color silver = Color(0xffcbcbcb);
+  static const Color dustyGray = Color(0xff999999);
+  static const Color goldenGlow = Color(0xfffde595);
+  static const Color yourPink = Color(0xffffc9c9);
+  static const Color athensGrey = Color(0xfff4f5f7);
+  static const Color doveGrey = Color(0xff6a6a6a);
+  static const Color emerald = Color(0xff41bf78);
+
+}
---
+++ cashop_frontend/lib/style/theme.dart
@@ -0,0 +1,27 @@
+import 'package:cashop_frontend/style/color_palette.dart';
+import 'package:flutter/material.dart';
+
+class CustomTheme{
+  static ColorScheme get colorScheme{
+    return const ColorScheme(
+      primary: ColorPalette.emerald, 
+      secondary: ColorPalette.algaeGreen,
+      onSecondary: ColorPalette.white,
+        onPrimary: ColorPalette.white, 
+        surface: ColorPalette.white,  background: ColorPalette.white, 
+        onBackground: ColorPalette.doveGrey, onSurface: ColorPalette.dustyGray, brightness: Brightness.light, 
+        error: ColorPalette.yourPink, onError: ColorPalette.white
+       );
+  }
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: ColorPalette.emerald,
+      primaryColorLight: ColorPalette.algaeGreen,
+      // primaryColorDark: ColorPalette.prussianBlue,
+      backgroundColor: ColorPalette.white,
+      scaffoldBackgroundColor: ColorPalette.white,
+      colorScheme: colorScheme,
+    );
+  }
+
+}
